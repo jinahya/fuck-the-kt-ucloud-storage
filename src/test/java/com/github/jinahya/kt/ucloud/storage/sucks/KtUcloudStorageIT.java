691980172d7e4b100a1d28d67f97bb9c6b623ae4
Jin Kwon <onacit@gmail.com>
2016-08-18 13:49:20 +0900

    updated

--- src/test/java/com/github/jinahya/kt/ucloud/storage/sucks/KtUcloudStorageIT.java
+++ src/test/java/com/github/jinahya/kt/ucloud/storage/sucks/KtUcloudStorageIT.java
@@ -16,13 +16,17 @@
 package com.github.jinahya.kt.ucloud.storage.sucks;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.testng.Assert.assertEquals;
@@ -48,6 +52,8 @@ public class KtUcloudStorageIT {
     private static String resellerAccount;
 
     private static String authKey;
+
+    private static final int INVOCATION_COUNT = 32;
 
     @BeforeClass
     public static void doBeforeClass() {
@@ -77,13 +83,8 @@ public class KtUcloudStorageIT {
         logger.debug("authKey: {}", authKey);
     }
 
-    @BeforeMethod
-    public void doBeforeMethod() {
-        logger.debug("-------------------------------------------------------");
-    }
-
-    private int statusCode(final HttpURLConnection connection,
-                           final int... expectedStatusCodes)
+    private static int statusCode(final HttpURLConnection connection,
+                                  final int... expectedStatusCodes)
             throws IOException {
         final int statusCode = connection.getResponseCode();
         final String reasonPhrase = connection.getResponseMessage();
@@ -99,14 +100,15 @@ public class KtUcloudStorageIT {
     }
 
     @DataProvider(name = "mediaTypes")
-    private Object[][] mediaTypes() {
+    private static Object[][] mediaTypes() {
         return new Object[][]{
             {"text/plain"}, {"application/xml"}, {"application/json"}
         };
     }
 
-    private void authenticateUser() throws IOException {
-        logger.debug("authenticateUser()");
+    @BeforeMethod
+    public void authenticateUser() throws IOException {
+        logger.debug("------------------------------------ authenticateUser()");
         final URL url = new URL(authUrl);
         final HttpURLConnection connection
                 = (HttpURLConnection) url.openConnection();
@@ -138,10 +140,53 @@ public class KtUcloudStorageIT {
         }
     }
 
-    @Test(dataProvider = "mediaTypes", invocationCount = 128)
+    @Test(invocationCount = INVOCATION_COUNT)
+    public void readAllContainers() throws IOException {
+        logger.debug("----------------------------------- readAllContainers()");
+        final int limit = 512;
+        final String[] marker = new String[1];
+        while (true) {
+            final StringBuilder spec = new StringBuilder(storageUrl);
+            spec.append('?')
+                    .append("limit").append('=')
+                    .append(Integer.toString(limit));
+            if (marker[0] != null) {
+                spec.append('&')
+                        .append("marker").append('=').append(marker[0]);
+            }
+            marker[0] = null;
+            final URL url = new URL(spec.toString());
+            final HttpURLConnection connection
+                    = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "text/plain");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 200, 204);
+                if (statusCode == 204) {
+                    break;
+                }
+                assertEquals(statusCode, 200);
+                try (InputStream in = connection.getInputStream();
+                     InputStreamReader r = new InputStreamReader(in, UTF_8);
+                     BufferedReader b = new BufferedReader(r)) {
+                    b.lines().forEach(l -> {
+//                        logger.debug("container: {}", l);
+                        marker[0] = l;
+                    });
+                }
+            } finally {
+                connection.disconnect();
+            }
+        }
+    }
+
+    @Test(dataProvider = "mediaTypes", invocationCount = INVOCATION_COUNT)
     public void readContainers(final String mediaType) throws IOException {
-        logger.debug("readContainers({})", mediaType);
-        authenticateUser();
+        logger.debug("------------------------- readContainers({})", mediaType);
         final URL url = new URL(storageUrl);
         final HttpURLConnection connection
                 = (HttpURLConnection) url.openConnection();
@@ -161,11 +206,170 @@ public class KtUcloudStorageIT {
                  InputStreamReader r = new InputStreamReader(in, UTF_8);
                  BufferedReader b = new BufferedReader(r)) {
                 b.lines().forEach(l -> {
-                    logger.debug("line: {}", l);
+//                    logger.debug("body: {}", l);
                 });
             }
         } finally {
             connection.disconnect();
+        }
+    }
+
+    // ------------------------------------------------------ /account/container
+    @Test(invocationCount = INVOCATION_COUNT)
+    public void verifyContainer() throws IOException {
+        logger.debug("------------------------------------- verifyContainer()");
+        final String container = getClass().getName() + "_" + UUID.randomUUID();
+        logger.debug("container: {}", container);
+        final URL url = new URL(storageUrl + "/" + container);
+        {
+            logger.debug("----------------------------- updating container...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 201, 202);
+                if (statusCode == 202) {
+                    return;
+                }
+            } finally {
+                connection.disconnect();
+            }
+        }
+        {
+            logger.debug("------------------------------ peeking container...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setRequestProperty("Accept", "*/*");
+            connection.setDoOutput(false);
+
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 204);
+            } finally {
+                connection.disconnect();
+            }
+        }
+        {
+            logger.debug("----------------------------- deleting container...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("DELETE");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 204);
+            } finally {
+                connection.disconnect();
+            }
+        }
+    }
+
+    // ----------------------------------------------- /account/container/object
+    @Test(invocationCount = INVOCATION_COUNT)
+    public void verifyObject() throws IOException {
+        logger.debug("---------------------------------------- verifyObject()");
+        final String container = getClass().getName() + "_" + UUID.randomUUID();
+        logger.debug("container: {}", container);
+        final String object = getClass().getName() + "_" + UUID.randomUUID();
+        logger.debug("object: {}", object);
+        final URL containerUrl = new URL(storageUrl + "/" + container);
+        final URL objectUrl = new URL(storageUrl + "/" + container + "/" + object);
+        {
+            logger.debug("----------------------------- updating container...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) containerUrl.openConnection();
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 201, 202);
+            } finally {
+                connection.disconnect();
+            }
+        }
+        final byte[] requestBody
+                = new byte[ThreadLocalRandom.current().nextInt(1024)];
+        ThreadLocalRandom.current().nextBytes(requestBody);
+        {
+            logger.debug("-------------------------------- updating object...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) objectUrl.openConnection();
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setChunkedStreamingMode(128);
+            try {
+                connection.connect();
+                try (OutputStream output = connection.getOutputStream()) {
+                    output.write(requestBody);
+                    output.flush();
+                }
+                final int statusCode = statusCode(connection, 201, 202);
+            } finally {
+                connection.disconnect();
+            }
+        }
+        {
+            logger.debug("--------------------------------- reading object...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) objectUrl.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 200);
+                final byte[] responseBody = new byte[requestBody.length];
+                try (DataInputStream stream
+                        = new DataInputStream(connection.getInputStream())) {
+                    stream.readFully(responseBody);
+                }
+                assertEquals(responseBody, requestBody);
+            } finally {
+                connection.disconnect();
+            }
+        }
+        {
+            logger.debug("-------------------------------- deleting object...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) objectUrl.openConnection();
+            connection.setRequestMethod("DELETE");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 204);
+            } finally {
+                connection.disconnect();
+            }
+        }
+        {
+            logger.debug("----------------------------- deleting container...");
+            final HttpURLConnection connection
+                    = (HttpURLConnection) containerUrl.openConnection();
+            connection.setRequestMethod("DELETE");
+            connection.setRequestProperty("X-Auth-Token", authToken);
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            try {
+                connection.connect();
+                final int statusCode = statusCode(connection, 204);
+            } finally {
+                connection.disconnect();
+            }
         }
     }
 
